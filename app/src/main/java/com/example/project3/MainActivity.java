fd0a6acbc60b7acc09e58f2af57486214fd4366b
Zarish Amin <zarishaminvirk@gmail.com>
2022-05-12 21:13:15 -0700

    final Project

---
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignup_activity();  }
+        });
+    }
+        public void openSignup_activity()
+        {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        }
+}
